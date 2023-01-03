83f0a03be30223006836bd60e0430bee44d19545
J <janawat0123@gmail.com>
2023-01-03 16:40:55 +0700

    เสร็จส่งงช้าหน่อยแต่ส่งง

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream score;
+    score.open(score.txt);
+    string x;
+    double sum=0;
+    double sumx2=0;
+    int i=0;
+    	while(getline(score.x)){
+        sum+=atof(x.c_str());
+        sumx2+=pow(atof(x.c_str()),2);
+        i++;
+        
+        
+        }
+	    
+    score.close();
+    cout << "Number of data = "<<i;
+    cout << setprecision(3);
+    cout << "Mean = "<<sum/i;
+    cout << "Standard deviation = "<<sqrt(((1.00/i)*sumx2)-pow(sum/i,2));
+}
